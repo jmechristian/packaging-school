46df3cb247517b74f0bcfefe6a296c4f59ffe152
jmechristian <jmechristian@gmail.com>
2024-11-11 08:44:55 -0500

    sssss

--- components/shared/CountdownTimer.js
+++ components/shared/CountdownTimer.js
@@ -2,7 +2,7 @@ import { AnimatePresence, motion } from 'framer-motion';
 import { useEffect, useRef, useState } from 'react';
 
 // NOTE: Change this date to whatever date you want to countdown to :)
-const COUNTDOWN_FROM = '12/09/2024';
+const COUNTDOWN_FROM = '12/04/2024';
 
 const SECOND = 1000;
 const MINUTE = SECOND * 60;
