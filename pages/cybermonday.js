6ef5b61138ab2867c3d7c51e52254f4b84b0be07
jmechristian <jmechristian@gmail.com>
2024-11-14 10:01:26 -0500

    ssss

--- pages/cybermonday.js
+++ pages/cybermonday.js
@@ -4,10 +4,17 @@ import LogoGrid from '../components/cybermonday/LogoGrid';
 import Features from '../components/cybermonday/Features';
 import CyberTestimonials from '../components/cybermonday/CyberTestimonials';
 import CyberCta from '../components/cybermonday/CyberCta';
+import Meta from '../components/shared/Meta';
 
 const Page = () => {
   return (
     <>
+      <Meta
+        title='Cyber Monday Sale!'
+        description='40% Off the Certificate of Packaging Science – Enroll Instantly, No
+          Application Required!'
+        image='https://packmedia54032-staging.s3.amazonaws.com/public/all-courses-seoimagewebp'
+      />
       <DarkGridHero />
       <LogoGrid />
       <Features />
