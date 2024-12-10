b971cdc35c2f81ffcb6056f55d3769957817235b
jmechristian <jmechristian@gmail.com>
2024-12-10 13:16:21 -0500

    ssss

--- pages/api/thinkific-signin.js
+++ pages/api/thinkific-signin.js
@@ -1,3 +1,23 @@
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' });
+  if (req.method === 'POST') {
+    console.log(req.body);
+
+    // Extract user information from the request body
+    const { first_name, last_name, company, email } = req.body.payload;
+
+    // Create a cookie value
+    const cookieValue = `first_name=${first_name}; last_name=${last_name}; company=${company}; email=${email}`;
+
+    // Set the cookie in the response header
+    res.setHeader(
+      'Set-Cookie',
+      `thinkific-user=${encodeURIComponent(cookieValue)}; Path=/; HttpOnly`
+    );
+
+    res.status(200).json({ name: 'John Doe' });
+  }
+
+  if (req.method === 'GET') {
+    res.status(200).json({ name: 'John Doe' });
+  }
 }
