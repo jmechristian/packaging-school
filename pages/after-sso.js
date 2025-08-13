da435077d8455b66dfd6dc1e7114a970e31c35c6
jmechristian <jmechristian@gmail.com>
2025-08-13 10:33:58 -0400

    dfdfdf

--- pages/after-sso.js
+++ pages/after-sso.js
@@ -1,13 +1,24 @@
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { useUser } from '@auth0/nextjs-auth0/client';
 
 export default function AfterSSO() {
   const router = useRouter();
+  const { userSetupComplete, awsUser, thinkificUser } = useSelector(
+    (state) => state.auth
+  );
+  const { user, isLoading: userIsLoading } = useUser();
+
   useEffect(() => {
     const { returnTo = '/' } = router.query;
     if (typeof returnTo === 'string') {
-      router.replace(returnTo);
+      // Wait for user setup to complete before redirecting
+      if (!userIsLoading && user && userSetupComplete && awsUser) {
+        router.replace(returnTo);
+      }
     }
-  }, [router]);
+  }, [router, user, userIsLoading, userSetupComplete, awsUser, thinkificUser]);
+
   return <div>Finishing login...</div>;
 }
