cdcab80ceb586f7f36b67659ae3fd850d4ac6a1a
jmechristian <jmechristian@gmail.com>
2025-09-23 11:56:57 -0400

    sssss

--- helpers/api.js
+++ helpers/api.js
@@ -125,6 +125,21 @@ export const packagingSchoolCourses = [
   '672c1d2b-ba6c-4e02-8c34-83e8c3e4f7b3',
 ];
 
+export const spcCourses = [
+  '5fc017b2-2149-40d7-a6d2-272afad2c4e3',
+  '13c3013a-a246-4a02-b974-9d2e6c2254df',
+  '02a237f0-3709-4c3a-8e02-47e04d8f1977',
+  'b8a62fef-5d63-4730-8cb5-0e0a9aec7486',
+  '4f1fdc1d-2a88-4ac3-9bd6-4b46a7b6ae08',
+  '8bc8c4ff-6c1d-40cd-9a2e-e9a71e826fa8',
+  '83b10e68-0bb0-44e6-a746-cfca4cbbe56e',
+  'e0dcbb7d-5a5a-4e36-a907-772a60766ccd',
+  '93ed9ccc-f7ab-49eb-b186-279e607101bb',
+  '6584feb9-665f-4ee8-9690-9a46e9fb7d41',
+  '0f5cad69-68eb-4af0-9ee6-af91ab1c8c22',
+  '97bd5164-0560-4116-a8a0-5f1e09c586ca',
+];
+
 export const LEVELS_CONFIG = [
   {
     level: 1,
---
+++ pages/church-and-dwight-spc/index.js
@@ -0,0 +1,428 @@
+import React, { useState, useEffect } from 'react';
+import Head from 'next/head';
+import Image from 'next/image';
+import dynamic from 'next/dynamic';
+import {
+  MdSearch,
+  MdOutlineTimer,
+  MdOutlineBook,
+  MdDownloadForOffline,
+} from 'react-icons/md';
+import { getCourseByID, getAllLearningOfTheMonths } from '../../helpers/api';
+import {
+  BoltIcon,
+  MinusIcon,
+  PlusIcon,
+  AcademicCapIcon,
+  BookmarkSquareIcon,
+} from '@heroicons/react/24/outline';
+import { Disclosure } from '@headlessui/react';
+const ReactGoogleSlides = dynamic(() => import('react-google-slides'), {
+  ssr: false,
+});
+import VideoPlayer from '../../components/VideoPlayer';
+import { spcCourses, createNewOrder } from '../../helpers/api';
+import { useThinkificLink } from '../../hooks/useThinkificLink';
+import { useSelector } from 'react-redux';
+import { useRouter } from 'next/router';
+
+const cpsExam = {
+  courseId: 'CPS-C13',
+  title: 'CPS Final Exam',
+  price: 399,
+  hours: 10,
+  lessons: 10,
+  subheadline: 'Test your knowledge with our comprehensive exam.',
+};
+
+const LOTMCard = ({ lesson }) => {
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    return date.toLocaleDateString('en-US', {
+      month: 'long',
+      day: 'numeric',
+      year: 'numeric',
+    });
+  };
+
+  return (
+    <div className='w-full h-full bg-[#f4f4f5] rounded-md pb-2 overflow-hidden'>
+      <div className='flex flex-col'>
+        <div
+          className='w-full aspect-[16/9] bg-black bg-cover bg-center'
+          style={{
+            backgroundImage: `url(${lesson.seoImage})`,
+          }}
+        ></div>
+        <div className='w-full flex flex-col gap-2 px-3 py-2'>
+          <div className='font-semibold leading-tight text-[#bf0031] w-full h-16 mt-1 line-clamp-3  max-w-[90%]'>
+            <span className='text-gray-700 leading-tight'>{lesson.title}</span>
+          </div>
+          <div className='w-full h-7 border-y border-gray-300 flex items-center justify-between text-gray-700'>
+            <div className='font-semibold text-xs'>
+              {formatDate(lesson.createdAt)}
+            </div>
+            <div className='font-semibold flex items-center gap-1'>
+              <MdOutlineTimer />
+            </div>
+          </div>
+          <div className='text-xs text-gray-700 h-20 mb-2 line-clamp-5'>
+            {lesson.subhead}
+          </div>
+          <div
+            className='w-full h-10 flex items-center justify-center bg-gray-900 text-white rounded-md cursor-pointer hover:bg-[#bf0031] transition-all duration-300'
+            onClick={() => {
+              window.open(`/lessons/${lesson.slug}`, '_blank');
+            }}
+          >
+            Read Lesson
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const CourseCard = ({ course }) => {
+  const [courseData, setCourseData] = useState(null);
+  const { awsUser } = useSelector((state) => state.auth);
+  const { navigateToThinkific } = useThinkificLink();
+  const router = useRouter();
+  useEffect(() => {
+    const fetchCourseData = async () => {
+      const data = await getCourseByID(course);
+      setCourseData(data);
+    };
+    fetchCourseData();
+  }, [course]);
+
+  const orderHandler = async () => {
+    const orderId = await createNewOrder({
+      courseDescription: courseData.subheadline,
+      courseDiscount: 100,
+      courseImage: courseData.seoImage,
+      courseName: courseData.title,
+      courseLink: `${courseData.link}?coupon=cdspc`,
+      total: courseData.price,
+      userID: awsUser ? awsUser.id : null,
+      email: awsUser ? awsUser.email : null,
+      name: awsUser ? awsUser.name : null,
+    });
+
+    if (awsUser && awsUser.name.includes(' ')) {
+      navigateToThinkific(
+        `${courseData.link}?coupon=cdspc`,
+        `${courseData.link}?coupon=cdspc`
+      );
+    } else {
+      router.push(`/order/${orderId.id}`);
+    }
+  };
+
+  return (
+    <div className='w-full h-full bg-[#f4f4f5] rounded-md pb-2 overflow-hidden'>
+      <div className='flex flex-col'>
+        <div
+          className='w-full aspect-[16/9] bg-black bg-cover bg-center'
+          style={{
+            backgroundImage: `url(${courseData && courseData.seoImage})`,
+          }}
+        >
+          {courseData && courseData.preview && (
+            <VideoPlayer
+              videoEmbedLink={courseData && courseData.preview}
+              light
+            />
+          )}
+        </div>
+        <div className='w-full flex flex-col gap-2 px-3 py-2'>
+          <div className='font-semibold leading-tight text-[0.9rem] text-[#bf0031] w-full h-10 mt-1 line-clamp-3 max-w-[80%]'>
+            {courseData && courseData.courseId}{' '}
+            <span className='text-gray-700'>
+              {courseData && courseData.title}
+            </span>
+          </div>
+          <div className='w-full h-7 border-y border-gray-300 flex items-center justify-between text-sm text-gray-700'>
+            <div className='flex items-center gap-1'>
+              <div className='font-semibold line-through text-gray-400'>
+                ${courseData && courseData.price}
+              </div>
+              <div className='font-semibold'>$0</div>
+            </div>
+
+            <div className='font-semibold flex items-center gap-1'>
+              {courseData && courseData.hours}
+              <MdOutlineTimer /> / {courseData && courseData.lessons}{' '}
+              <MdOutlineBook />
+            </div>
+          </div>
+          <div className='text-xs text-gray-700 h-20 mb-2 line-clamp-5'>
+            {courseData && courseData.shortDescription
+              ? courseData.shortDescription
+              : courseData && courseData.subheadline}
+          </div>
+          <div
+            className='w-full h-10 flex items-center justify-center bg-gray-900 text-white rounded-md cursor-pointer hover:bg-[#bf0031] transition-all duration-300'
+            onClick={orderHandler}
+          >
+            Begin Course
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const Page = () => {
+  const faqs = [
+    {
+      id: 1,
+      question: 'Who is my Church & Dwight contact?',
+      answer:
+        'If you have any questions regarding curriculum, reach out to michael.pinchiaroli@churchdwight.com. ',
+    },
+    {
+      id: 2,
+      question: 'What if I run into technical difficulties?',
+      answer:
+        'We are happy to help at The Packaging School—email info@packagingschool.com.',
+    },
+    {
+      id: 3,
+      question: 'How do I sign up?',
+      answer:
+        'Refer to the slide deck / PDF at the top of the page or email info@packagingschool.com.',
+    },
+  ];
+  const [searchQuery, setSearchQuery] = useState('');
+  const [learningOfTheMonthQuery, setLearningOfTheMonthQuery] = useState('');
+  const [learningOfTheMonths, setLearningOfTheMonths] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 8;
+
+  useEffect(() => {
+    const fetchLearningOfTheMonths = async () => {
+      const data = await getAllLearningOfTheMonths();
+      setLearningOfTheMonths(data);
+    };
+    fetchLearningOfTheMonths();
+  }, []);
+
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+
+  // Add filtering before pagination
+  const filteredItems = learningOfTheMonths.filter(
+    (lesson) =>
+      lesson.title
+        .toLowerCase()
+        .includes(learningOfTheMonthQuery.toLowerCase()) ||
+      lesson.subhead
+        .toLowerCase()
+        .includes(learningOfTheMonthQuery.toLowerCase())
+  );
+
+  const currentItems = filteredItems.slice(indexOfFirstItem, indexOfLastItem);
+  const totalPages = Math.ceil(filteredItems.length / itemsPerPage);
+
+  return (
+    <div className='w-full flex flex-col pt-10 pb-40'>
+      <Head>
+        <title>Church & Dwight | The Packaging School</title>
+        <meta name='robots' content='noindex,nofollow' />
+      </Head>
+
+      <div className='w-full max-w-7xl mx-auto grid grid-cols-12 items-center relative'>
+        <div
+          className='bg-[#bf0031] rounded-lg h-[240px] row-span-full col-start-1 col-span-9 self-center bg-cover bg-center flex items-center'
+          style={{
+            backgroundImage:
+              'url(https://packschool.s3.us-east-1.amazonaws.com/cd-back.png)',
+          }}
+        >
+          <div className='w-1/2 flex gap-5 px-6 items-center'>
+            <div className='w-[380px]'>
+              <Image
+                src={
+                  'https://packschool.s3.us-east-1.amazonaws.com/Church+and+Dwight_idGkwhDpFy_1.png'
+                }
+                alt='church-and-dwight-logo'
+                width={800}
+                height={807}
+              />
+            </div>
+            <div className='text-white h2-base'>
+              Welcome, <span className='text-[#FFA300]'>Church & Dwight!</span>
+            </div>
+          </div>
+        </div>
+        <div className='w-full flex flex-col bg-[#f4f4f5] rounded-lg aspect-[16/9] row-span-full col-span-6 col-start-7 self-end absolute top-[60%]'>
+          <div className='w-full aspect-[16/9]'>
+            <ReactGoogleSlides
+              width={'100%'}
+              height={'100%'}
+              slidesLink={
+                'https://docs.google.com/presentation/d/1Anh_TCkYQaRUomG2Bd8x5wpfpDlKy81NuMCV-buafXA/edit?usp=drivesdk'
+              }
+              position={1}
+              showControls
+              loop
+            />
+            <div
+              className='flex items-center gap-1 justify-center mt-2 cursor-pointer'
+              onClick={() => {
+                window.open(
+                  'https://packschool.s3.us-east-1.amazonaws.com/C%26D+Library+Instructions.pdf',
+                  '_blank'
+                );
+              }}
+            >
+              <div className='text-gray-700'>Download as PDF</div>
+              <MdDownloadForOffline className='text-gray-700' size={20} />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='w-full max-w-7xl mx-auto flex flex-col gap-10 pt-8 pb-16 border-b border-gray-300'>
+        <div className='w-full flex flex-col gap-5 mt-12 lg:mt-0'>
+          <div className='max-w-xl w-full text-gray-700'>
+            Church & Dwight is committed to the professional growth of its
+            employees, recognizing that continuous learning is essential to
+            innovation and long-term success. As part of this commitment, we’re
+            excited to offer access to the Sustainable Packaging Coalition (SPC)
+            Knowledge Library, developed in partnership with The Packaging
+            School.
+          </div>
+          <div className='max-w-xl w-full text-gray-700'>
+            This exclusive resource is available at no cost to Church & Dwight
+            employees through our company’s SPC membership. The Knowledge
+            Library is designed to support your development in sustainable
+            packaging—whether you’re building a foundation or diving into
+            advanced topics.
+          </div>
+        </div>
+      </div>
+      <div className='w-full max-w-7xl mx-auto flex flex-col gap-10 px-10 pt-5 pb-5 border-b border-gray-300'>
+        <div className='w-full flex items-center justify-between'>
+          <div className='leading-snug max-w-lg w-full text-xl font-bold text-gray-700'>
+            Your Courses
+          </div>
+          <div className='relative flex items-center'>
+            <input
+              type='text'
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+              placeholder='Search courses...'
+              className='pl-10 pr-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-[#bf0031] focus:border-transparent'
+            />
+            <MdSearch className='absolute left-3 text-gray-400 text-xl' />
+          </div>
+        </div>
+      </div>
+      <div className='w-full max-w-7xl mx-auto flex flex-col gap-10 px-10 pt-8 pb-8 border-b border-gray-300'>
+        <div className='w-full flex items-center justify-center max-w-7xl mx-auto bg-[#bf0031] px-3'>
+          <div className='text-white py-1.5 rounded font-bold'>
+            Company funds have enabled this course access —you may not enroll
+            anonymously or with private email addresses
+          </div>
+        </div>
+        <div className='grid md:grid-cols-2 lg:grid-cols-4 gap-8'>
+          {spcCourses.map((course) => (
+            <CourseCard key={course} course={course} />
+          ))}
+        </div>
+      </div>
+      <div className='w-full max-w-7xl mx-auto flex flex-col gap-10 px-10 pt-5 pb-5 border-b border-gray-300'>
+        <div className='w-full flex items-center justify-between'>
+          <div className='leading-snug max-w-lg w-full text-xl font-bold text-gray-700'>
+            Your Learning of the Month
+          </div>
+          <div className='relative flex items-center'>
+            <input
+              type='text'
+              value={learningOfTheMonthQuery}
+              onChange={(e) => setLearningOfTheMonthQuery(e.target.value)}
+              placeholder='Search courses...'
+              className='pl-10 pr-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-[#bf0031] focus:border-transparent'
+            />
+            <MdSearch className='absolute left-3 text-gray-400 text-xl' />
+          </div>
+        </div>
+      </div>
+      <div className='w-full max-w-7xl mx-auto flex flex-col gap-10 px-10 pt-8 pb-8 border-b border-gray-300'>
+        <div className='grid md:grid-cols-2 lg:grid-cols-4 gap-8'>
+          {currentItems.map((lesson) => (
+            <LOTMCard key={lesson.id} lesson={lesson} />
+          ))}
+        </div>
+        {totalPages > 1 && (
+          <div className='flex justify-center items-center gap-4 mt-8'>
+            <button
+              onClick={() => setCurrentPage((prev) => Math.max(prev - 1, 1))}
+              disabled={currentPage === 1}
+              className={`flex items-center gap-1 ${
+                currentPage === 1
+                  ? 'text-gray-400 cursor-not-allowed'
+                  : 'text-gray-700 hover:text-[#bf0031]'
+              } transition-all duration-300`}
+            >
+              ←
+            </button>
+            <span className='text-gray-700'>
+              {currentPage} / {totalPages}
+            </span>
+            <button
+              onClick={() =>
+                setCurrentPage((prev) => Math.min(prev + 1, totalPages))
+              }
+              disabled={currentPage === totalPages}
+              className={`flex items-center gap-1 ${
+                currentPage === totalPages
+                  ? 'text-gray-400 cursor-not-allowed'
+                  : 'text-gray-700 hover:text-[#bf0031]'
+              } transition-all duration-300`}
+            >
+              →
+            </button>
+          </div>
+        )}
+      </div>
+      <div className='mx-auto divide-y divide-gray-900/10 w-full max-w-7xl bg-[#f4f4f5] rounded-lg p-10 mt-10'>
+        <h2 className='text-2xl font-bold leading-10 tracking-tight text-gray-900'>
+          Frequently asked questions
+        </h2>
+        <dl className='mt-10 space-y-6 divide-y divide-gray-900/10'>
+          {faqs.map((faq) => (
+            <Disclosure as='div' key={faq.question} className='pt-6'>
+              {({ open }) => (
+                <>
+                  <dt>
+                    <Disclosure.Button className='flex w-full items-start justify-between text-left text-gray-900'>
+                      <span className='text-base font-semibold leading-7'>
+                        {faq.question}
+                      </span>
+                      <span className='ml-6 flex h-7 items-center'>
+                        {open ? (
+                          <MinusIcon className='h-6 w-6' aria-hidden='true' />
+                        ) : (
+                          <PlusIcon className='h-6 w-6' aria-hidden='true' />
+                        )}
+                      </span>
+                    </Disclosure.Button>
+                  </dt>
+                  <Disclosure.Panel as='dd' className='mt-2 pr-12'>
+                    <p className='text-base leading-7 text-gray-600'>
+                      {faq.answer}
+                    </p>
+                  </Disclosure.Panel>
+                </>
+              )}
+            </Disclosure>
+          ))}
+        </dl>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
