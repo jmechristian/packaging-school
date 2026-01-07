02d8dd9acf8973a56bc34f4a68a684c4e4be9d06
jmechristian <jmechristian@gmail.com>
2026-01-07 10:24:16 -0500

    ssss

--- pages/forms/cmpm/icpf/[fid].js
+++ pages/forms/cmpm/icpf/[fid].js
@@ -16,7 +16,7 @@ const Page = ({ pageData }) => {
         <CenteredTextHeader
           headline='Certificate of Mastery in Packaging Management'
           heading='Sponsored by ICPF'
-          subhead='Whether you’re a packaging major or exploring new fields, this program introduces you to the corrugated industry and equips you with practical skills that set you apart and prepare you for success in the exciting, fast-growing world of packaging. Fill out the form below to get started. You will receive a confirmation email within 1–3 business days of your complete application submission. We are looking forward to getting to know you better and cannot wait to share our perspective with you on how packaging is an awesome industry full of opportunity.'
+          subhead='Whether you’re pursuing a STEAM major or exploring how your skills apply across industries, this program introduces you to the corrugated and packaging space and equips you with hands-on, real-world experience that helps you stand out and prepare for career success. Fill out the form below to get started. You will receive a confirmation email within 1–3 business days of your complete application submission. We are looking forward to getting to know you better and cannot wait to share our perspective with you on how packaging is an awesome industry full of opportunity.'
         />
         <CMPMWrapper params={pageData.getIcpfCmpmForm} />
       </div>
