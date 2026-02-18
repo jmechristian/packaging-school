dc1e5401412c4eabae58879f6bd9e5c8a4376477
jmechristian <jmechristian@gmail.com>
2026-02-18 06:13:12 -0500

    script updates

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import '../styles/globals.css';
+import Script from 'next/script';
 import Layout from '../features/layout/Layout';
 import { store } from '../features/store';
 import { Provider } from 'react-redux';
@@ -12,6 +13,35 @@ Amplify.configure(awsExports);
 export default function App({ Component, pageProps }) {
   return (
     <UserProvider>
+      <Script
+        strategy='lazyOnload'
+        src='https://www.googletagmanager.com/gtag/js?id=G-02DBGHP71V'
+      />
+      <Script
+        id='google-analytics'
+        strategy='lazyOnload'
+        dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5H8KXB9');`,
+        }}
+      />
+      <Script
+        id='hotjar'
+        strategy='lazyOnload'
+        dangerouslySetInnerHTML={{
+          __html: `(function(h,o,t,j,a,r){
+h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+h._hjSettings={hjid:3554961,hjsv:6};
+a=o.getElementsByTagName('head')[0];
+r=o.createElement('script');r.async=1;
+r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+a.appendChild(r);
+})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
+        }}
+      />
       <Provider store={store}>
         <DefaultMeta />
         <Layout>
--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,5 +1,4 @@
 import { Html, Head, Main, NextScript } from 'next/document';
-import Script from 'next/script';
 
 export default function Document() {
   return (
@@ -17,35 +16,8 @@ export default function Document() {
           href='https://fonts.gstatic.com'
           crossOrigin='true'
         />
-        <Script
-          strategy='afterInteractive'
-          src='https://www.googletagmanager.com/gtag/js?id=G-02DBGHP71V'
-        />
-        <Script
-          id='google-analytics'
-          strategy='afterInteractive'
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-5H8KXB9');`,
-          }}
-        />
-        <Script
-          id='hotjar'
-          strategy='afterInteractive'
-          dangerouslySetInnerHTML={{
-            __html: `(function(h,o,t,j,a,r){
-              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-              h._hjSettings={hjid:3554961,hjsv:6};
-              a=o.getElementsByTagName('head')[0];
-              r=o.createElement('script');r.async=1;
-              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-              a.appendChild(r);
-            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
-          }}
-        />
+        <link rel='preconnect' href='https://www.googletagmanager.com' />
+        <link rel='preconnect' href='https://static.hotjar.com' crossOrigin='' />
         <link
           rel='preload'
           href='https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap'
