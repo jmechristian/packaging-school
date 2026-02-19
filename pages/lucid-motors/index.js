0fd7e8e7f40e00bea2a1ccbd6a2258b8677ecfac
jmechristian <jmechristian@gmail.com>
2026-02-19 12:28:19 -0500

    sdsdsd

--- pages/lucid-motors/index.js
+++ pages/lucid-motors/index.js
@@ -456,6 +456,25 @@ const Page = ({ lib, learningOfTheMonths }) => {
               searchQuery=''
             />
           ))}
+          <CourseCard
+            key='APC-00'
+            course='APC-00'
+            courseData={{
+              id: 'APC-00',
+              title: 'Automotive Packaging Certificate Bundle',
+              subheadline:
+                'Enroll in all 10 courses and pass the final exam to earn your certification. The fastest way to get your certification.',
+              link: 'https://packagingschool.com/enroll/623256?price_id=659662&coupon=lucidlibrary',
+              hours: '29',
+              lessons: '151',
+              videos: '10',
+              price: '2400',
+              seoImage:
+                'https://files.cdn.thinkific.com/bundles/bundle_card_image_000/031/389/1735935575.original.jpg',
+              slug: 'apc-bundle',
+            }}
+            searchQuery=''
+          />
         </div>
       </div>
       <div
@@ -489,6 +508,25 @@ const Page = ({ lib, learningOfTheMonths }) => {
               searchQuery=''
             />
           ))}
+          <CourseCard
+            key='CSP-00'
+            course='CSP-00'
+            courseData={{
+              id: 'CSP-00',
+              title: 'Certificate of Sustainable Packaging Bundle',
+              subheadline:
+                'Enroll in all 6 courses and pass the final exam to earn your certification. The fastest way to get your certification.',
+              link: 'https://packagingschool.com/enroll/3686568?price_id=4633347&coupon=lucidlibrary',
+              hours: '10',
+              lessons: '33',
+              videos: '5',
+              price: '2400',
+              seoImage:
+                'https://files.cdn.thinkific.com/bundles/bundle_card_image_000/358/972/1771518762.original.png',
+              slug: 'csp-bundle',
+            }}
+            searchQuery=''
+          />
         </div>
       </div>
       <div
@@ -520,6 +558,25 @@ const Page = ({ lib, learningOfTheMonths }) => {
               searchQuery=''
             />
           ))}
+          <CourseCard
+            key='CPS-00'
+            course='CPS-00'
+            courseData={{
+              id: 'CPS-00',
+              title: 'Certificate of Packaging Science Bundle',
+              subheadline:
+                'Enroll in all 12 courses and pass the final exam to earn your certification. The fastest way to get your certification.',
+              link: 'https://packagingschool.com/enroll/3686568?price_id=4633347&coupon=lucidlibrary',
+              hours: '70',
+              lessons: '773',
+              videos: '10',
+              price: '2400',
+              seoImage:
+                'https://files.cdn.thinkific.com/bundles/bundle_card_image_000/003/803/1507034685.original.jpg',
+              slug: 'cps-00',
+            }}
+            searchQuery=''
+          />
         </div>
       </div>
       {/* LOTM */}
